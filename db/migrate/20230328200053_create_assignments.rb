5278ec37961004c64ae42f0bdf205d3ac195c61c
Jasmine Nie <junn@andrew.cmu.edu>
2023-04-25 14:32:50 -0400

    removed parent reference

--- db/migrate/20230328200053_create_assignments.rb
+++ db/migrate/20230328200053_create_assignments.rb
@@ -2,7 +2,6 @@ class CreateAssignments < ActiveRecord::Migration[5.2]
   def change
     create_table :assignments do |t|
       t.references :item, foreign_key: true
-      t.references :parent, foreign_key: true
       t.date :due_date
       t.boolean :completion
       t.string :status
